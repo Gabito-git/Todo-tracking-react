4ae940920aa83355e0755bcf64e3dc69d242a500
Gabriel <luisgabrielarevalo@hotmail.com>
2021-02-09 09:49:32 -0500

    Router delete 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,15 @@
 import Button from "./Button";
-import { useLocation } from "react-router-dom";
 
-
-const Header = ({ setBtn, btnState }) => {
-
-    const location = useLocation();
+const Header = ({ setBtn, btnState }) => { 
 
     return (
         <header className="header">
             <h1 className="header__title">Task Tracker</h1>
-            { location.pathname ==='/' && <Button
-                                            setBtn={ setBtn }
-                                            btnState={ btnState }
-                                          />
-            }
+            <Button
+                setBtn={ setBtn }
+                btnState={ btnState }
+            />
+            
         </header>
     )
 }
